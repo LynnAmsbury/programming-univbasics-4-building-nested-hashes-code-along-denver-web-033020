62628e4907c8af394741e57c3dc26057bb593648
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-01 19:39:09 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -11,9 +11,8 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {}
       :matriarch => {}
-      :hero => {}
-      :hero_friends => []
-      }
+      :heroine => {}
+      :heroine_friends => []
       }
     }
 
